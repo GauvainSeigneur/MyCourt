98b059b1cac10226e72fd165010eddf167e223a5
gauvains <gauvain.seigneur@gmail.com>
2018-09-25 09:03:09 +0200

    small update

--- app/src/main/java/seigneur/gauvain/mycourt/data/viewModel/ShotDraftViewModel.java
+++ app/src/main/java/seigneur/gauvain/mycourt/data/viewModel/ShotDraftViewModel.java
@@ -10,19 +10,17 @@ import java.util.List;
 import javax.inject.Inject;
 
 import io.reactivex.disposables.CompositeDisposable;
+import seigneur.gauvain.mycourt.data.model.Shot;
 import seigneur.gauvain.mycourt.data.model.ShotDraft;
 import seigneur.gauvain.mycourt.data.model.User;
 import seigneur.gauvain.mycourt.data.repository.ShotDraftRepository;
+import seigneur.gauvain.mycourt.data.repository.TempDataRepository;
 import seigneur.gauvain.mycourt.data.repository.UserRepository;
 import seigneur.gauvain.mycourt.utils.ConnectivityReceiver;
 import timber.log.Timber;
 
 public class ShotDraftViewModel extends ViewModel {
 
-    @Inject
-    ShotDraftRepository mShotDraftRepository;
-
-    private final CompositeDisposable disposables = new CompositeDisposable();
 
     @Inject
     public ShotDraftViewModel() {}
@@ -30,21 +28,12 @@ public class ShotDraftViewModel extends ViewModel {
     @Override
     public void onCleared(){
         super.onCleared();
-        disposables.clear();
     }
 
     /*
      *********************************************************************************************
      * PUBLIC METHODS CALLED IN PRESENTER
      *********************************************************************************************/
-    public void init() {
-        if (getShotDrafts() !=null)
-            return;
-    }
 
-    public LiveData<List<ShotDraft>> getShotDrafts() {
-        return LiveDataReactiveStreams.fromPublisher(
-                mShotDraftRepository.getShotDraft().toFlowable());
-    }
 
 }
